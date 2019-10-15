758e525cf674146466954f199f0cbcde5ba254ca
kei0822kei <mizokami.keiyu@gmail.com>
2019-10-15 17:09:46 +0900

    add docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'travis'
+project = 'test_document'
 copyright = '2019, Keiyu Mizokami'
 author = 'Keiyu Mizokami'
 
@@ -39,7 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
